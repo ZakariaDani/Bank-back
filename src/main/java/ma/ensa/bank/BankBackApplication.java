1181be74e3e77533422444157516c3565111a833
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-05-01 13:23:11 +0000

    first-commit

---
+++ src/main/java/ma/ensa/bank/BankBackApplication.java
@@ -0,0 +1,13 @@
+package ma.ensa.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/ensa/bank/BankBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.ensa.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
