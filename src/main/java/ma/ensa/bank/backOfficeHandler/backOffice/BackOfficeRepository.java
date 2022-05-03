e6998de2544cacbecf254b1b425798dd8258a193
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-05-03 23:00:52 +0000

    add back-office-handler

---
+++ src/main/java/ma/ensa/bank/backOfficeHandler/backOffice/BackOfficeRepository.java
@@ -0,0 +1,13 @@
+package ma.ensa.bank.backOfficeHandler.backOffice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface BackOfficeRepository extends JpaRepository<BackOffice, String> {
+    Optional<BackOffice> findByEmail(String email);
+}
