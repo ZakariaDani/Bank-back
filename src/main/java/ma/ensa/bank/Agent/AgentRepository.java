0f5ffda9257c230cfe071f74dd8a809981cc0204
marouane <marwanezibout10@gmail.com>
2022-05-04 01:31:20 +0000

    agent-crud-backend

---
+++ src/main/java/ma/ensa/bank/Agent/AgentRepository.java
@@ -0,0 +1,23 @@
+package ma.ensa.bank.Agent;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface AgentRepository extends JpaRepository<Agent, String> {
+
+    @Query("SELECT s FROM Agent s WHERE s.email=?1")
+    Optional<Agent> findAgentByEmail(String email);
+
+    @Query("SELECT s FROM Agent s WHERE s.idCardNumber=?1")
+    Optional<Agent> findAgentById(String agentIdCard);
+
+    @Query("SELECT s FROM Agent s WHERE s.phone=?1")
+    Optional<Agent> findAgentByPhone(String phone);
+
+}
