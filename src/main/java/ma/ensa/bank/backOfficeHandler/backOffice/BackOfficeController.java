42f7e5dbdb5a6ead315561a5089d8ab6400fa1fc
Aymane Daif <aymaned345@gmail.com>
2022-06-10 00:20:30 +0100

    :sparkles: add agent's id image

--- src/main/java/ma/ensa/bank/backOfficeHandler/backOffice/BackOfficeController.java
+++ src/main/java/ma/ensa/bank/backOfficeHandler/backOffice/BackOfficeController.java
@@ -5,10 +5,14 @@ import ma.ensa.bank.ClientHandler.Client.Client;
 import ma.ensa.bank.agentHandler.agent.Agent;
 import ma.ensa.bank.agentHandler.agent.AgentDTO;
 import ma.ensa.bank.agentHandler.agent.AgentService;
+import ma.ensa.bank.image.ImageService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +23,7 @@ import java.util.Optional;
 public class BackOfficeController {
     private final BackOfficeService backOfficeService;
     private final AgentService agentService;
+    private final ImageService imageService;
 
     @GetMapping
     public ResponseEntity<List<Agent>> getAgents() {
@@ -41,10 +46,10 @@ public class BackOfficeController {
     }
 
     @PostMapping
-    public ResponseEntity<?> createAgent(@RequestBody AgentDTO agentDTO) {
+    public ResponseEntity<?> createAgent(@RequestBody AgentDTO agentDTO, @RequestParam MultipartFile file) {
         if (agentDTO == null)
             return ResponseEntity.badRequest().body("The provided agent is not valid");
-        Agent newAgent = backOfficeService.saveAgent(agentDTO);
+        Agent newAgent = backOfficeService.saveAgent(agentDTO, file);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(newAgent);
@@ -52,7 +57,7 @@ public class BackOfficeController {
 
 
     @PatchMapping ("/{id}")
-    public ResponseEntity<?> updateAgent(@PathVariable("id") final Long id, @RequestBody AgentDTO agentDTO) {
+    public ResponseEntity<?> updateAgent(@PathVariable("id") final Long id, @RequestBody AgentDTO agentDTO, @RequestParam MultipartFile file) {
         System.out.println("here");
         if (agentDTO == null)
             return ResponseEntity.badRequest().body("The provided agent is not valid");
@@ -61,7 +66,7 @@ public class BackOfficeController {
         if(existedAgent.isPresent()) {
             Agent currentAgent = existedAgent.get();
 
-            Agent newAgent = backOfficeService.updateAgent(currentAgent, agentDTO);
+            Agent newAgent = backOfficeService.updateAgent(currentAgent, agentDTO, file);
             return ResponseEntity
                     .ok()
                     .body(newAgent);
@@ -97,5 +102,14 @@ public class BackOfficeController {
             return ResponseEntity.badRequest().body("The given agent is not valid");
         backOfficeService.deleteAgent(agentEmail);
         return ResponseEntity.ok().body(agentService.getAgentByEmail(agentEmail));
+    }
+
+
+    @GetMapping(
+            value = "image/{imageName}",
+            produces = {MediaType.IMAGE_JPEG_VALUE,MediaType.IMAGE_GIF_VALUE,MediaType.IMAGE_PNG_VALUE}
+    )
+    public @ResponseBody byte[] getImageWithMediaType(@PathVariable("imageName") String fileName) throws IOException {
+        return this.imageService.getImage(fileName);
     }
 }
---
+++ src/main/java/ma/ensa/bank/image/ImageService.java
@@ -0,0 +1,48 @@
+package ma.ensa.bank.image;
+
+import org.apache.commons.io.IOUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+@Service
+public class ImageService {
+    /*
+    * path where we will store the image
+    * */
+    public final String directoryPath = "C:\\images";
+
+    public String uploadImage(MultipartFile file) {
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path directory = Paths.get(directoryPath);
+        if(!Files.exists(directory)){
+            try {
+                Files.createDirectories(directory);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+
+        Path imagePath = Paths.get(directory + "\\" + fileName);
+
+        try {
+            Files.copy(file.getInputStream(), imagePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return imagePath.toString();
+    }
+
+    public  byte[] getImage(String imageName) throws IOException {
+        Path destination = Paths.get(directoryPath+"\\"+imageName);
+        return IOUtils.toByteArray(destination.toUri());
+    }
+
+}
