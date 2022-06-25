422a47bd10875e399ca1c1a3e55924a081e6c0f6
unknown <youssefbahomman3@gmail.com>
2022-06-25 19:57:48 +0100

    sending password to client

--- src/main/java/ma/ensa/bank/ClientHandler/Client/ClientService.java
+++ src/main/java/ma/ensa/bank/ClientHandler/Client/ClientService.java
@@ -181,9 +181,37 @@ public class ClientService {
     }
     @Transactional
     public void assign_client_to_agent(Long idClient,String emailAgent){
+        System.out.println("work");
         Client clientDb = clientRepository.findClientById(idClient).get();
         Agent agent = agentRepository.findAgentByEmail(emailAgent).get();
+        this.sendCodeToClient(clientDb.getId());
         clientDb.setAgent(agent);
+    }
+    public String sendCodeToClient(Long id){
+        System.out.println("email is sending...");
+        Optional<Client> c = this.getClientById(id);
+        if(c.isPresent()){
+            System.out.println("client found...");
+            Client client  = c.get();
+            String password = new Random().ints(10, 33, 122).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+            String EncodePass = PasswordEncoder.bCryptPasswordEncoder().encode(password);
+            client.setPassword(EncodePass);
+            this.updateClient(id, client);
+            System.out.println("client updated");
+            String message = "Hello Mr" + client.getFirstName() + client.getLastName() + "Your New Password is: " + password;
+            EmailEntity emailEntity = new EmailEntity(client.getEmail(), "your New Password", message);
+
+            try {
+                emailService.sendMail(emailEntity);
+                System.out.println("email is sending to: "+client.getEmail()+"\nmessage is: "+ emailEntity);
+                return "Succes";
+            }catch (Exception e){
+                e.printStackTrace();
+                return "Error";
+            }
+        }
+        return "Error";
     }
     public void deleteClient(Long id){
         Optional<Client> opt1 = clientRepository.findClientById(id);
@@ -205,26 +233,7 @@ public class ClientService {
 
     public Optional<Client> getClientById(Long Id){ return clientRepository.findClientById(Id);};
 
-    public String sendCodeToClient(Long id){
-        Optional<Client> c = this.getClientById(id);
-        if(c.isPresent()){
-            Client client  = c.get();
-            String password = new Random().ints(10, 33, 122).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                    .toString();
-            client.setPassword(password);
-            this.updateClient(id, client);
-            String message = "Hello Mr" + client.getFirstName() + client.getLastName() + "Your New Password is: " + client.getPassword();
-            EmailEntity emailEntity = new EmailEntity(client.getEmail(), "your New Password", message);
-            try {
-                emailService.sendMail(emailEntity);
-                return "Succes";
-            }catch (Exception e){
-                e.printStackTrace();
-                return "Error";
-            }
-        }
-        return "Error";
-    }
+
     public List<Client> getClientsByAgentId(Long agentId){
         return clientRepository.getClientsByAgentId(agentId).get();
     }
